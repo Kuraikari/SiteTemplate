9297b207e360b0eea4465f26fac409aafee0308d
Zian  Wermelinger <zwermi@gmail.com>
2017-03-21 22:57:35 +0100

    Update site.php

--- php/site.php
+++ php/site.php
@@ -1,1 +1,18 @@
+<?php
+class Site(){
+  public head;
+  public nav;
+  public content;
+  public footer;
+ 
+  
+  public Site(){
+    head = NULL;
+    nav = NULL;
+    content = NULL;
+    footer = NULL;  
+  }
 
+
+}
+?>

4a0f0a3bdc796b52f10fd246213cef3bae668826
Kuraikari <zwermi@gmail.com>
2017-03-22 18:22:02 +0100

    Different classes for the site
    I started to write some basic classes for the html.

--- php/site.php
+++ php/site.php
@@ -1,18 +1,65 @@
 <?php
-class Site(){
-  public head;
-  public nav;
-  public content;
-  public footer;
+public class Site(){
+    public $head;
+    public $nav;
+    public $content;
+    public $footer;
  
   
-  public __construct(){
-    head = NULL;
-    nav = NULL;
-    content = NULL;
-    footer = NULL;  
-  }
+    public __construct($head, $nav, $content, $footer){
+        $this->head = $head;
+        $this->nav = $nav;
+        $this->content = $content;
+        $this->footer = $footer;  
+    }
+    
+    public function createSite(){
+    
+    }
 
 
+}
+
+
+public class Head(){
+    public $css;
+    public $meta = array("Author", "Version","Charset");
+    public $script;
+
+    
+    public __construct(){
+        
+    }
+}
+
+public class CSS(){
+    public $path;
+    public $name;
+    public $type;
+    
+    public css;
+    
+    public __construct($path, $name, $type){
+        $this->path = $path;
+        $this->name = $name;
+        $this->type = $type;
+    }
+    
+    public function createCSSWithoutType(){
+        css = "<link rel='stylesheet' href='"$path+"/"+$name"'>";
+    }
+    
+    public function createCSSWithType(){
+        css = "<link rel='stylesheet' type='"$type"' href='"$path+"/"+$name"'>";
+    }
+    
+}
+
+public class Meta(){
+    
+}
+
+public class Script(){
+    
 }
 ?>

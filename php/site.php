236c9bdacd0285373c115bd762f11b299d02b9eb
Zian  Wermelinger <zwermi@gmail.com>
2017-03-23 10:12:07 +0100

    Update site.php

--- php/site.php
+++ php/site.php
@@ -19,47 +19,4 @@ public class Site(){
 
 
 }
-
-
-public class Head(){
-    public $css;
-    public $meta = array("Author", "Version","Charset");
-    public $script;
-
-    
-    public __construct(){
-        
-    }
-}
-
-public class CSS(){
-    public $path;
-    public $name;
-    public $type;
-    
-    public css;
-    
-    public __construct($path, $name, $type){
-        $this->path = $path;
-        $this->name = $name;
-        $this->type = $type;
-    }
-    
-    public function createCSSWithoutType(){
-        css = "<link rel='stylesheet' href='"$path+"/"+$name"'>";
-    }
-    
-    public function createCSSWithType(){
-        css = "<link rel='stylesheet' type='"$type"' href='"$path+"/"+$name"'>";
-    }
-    
-}
-
-public class Meta(){
-    
-}
-
-public class Script(){
-    
-}
 ?>

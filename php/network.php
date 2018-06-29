afad230f7cd098248fb9308a245b73a738e08501
Kuraikari <zwermi@gmail.com>
2018-06-29 14:47:52 +0200

    working with phpstorm now
    now working on the network and switched from mysqli to pdo.

--- php/network.php
+++ php/network.php
@@ -1,26 +1,61 @@
 <?php
 class Network{
-    public $connection;
-    public $security;
+    protected $_connection;
+    protected $_query;
+    protected $_session;
+    protected $_query_string;
 
-    public static function createConnection($servername, $username, $password){
-        $this->connection = new mysqli($servername, $username, $password);
-        if ($this->connection->connect_error) {
-          die("Connection failed: " . $this->connection->connect_error);
+    public $data;
+
+    /**
+     * @param $target_ip
+     * @param $target_db
+     * @param $user
+     * @param string $password
+     * @return $this
+     */
+    public function create_Connection($target_ip, $target_db, $user, $password=""){
+        try {
+            $this->_connection = new PDO("mysql:host=$target_ip;dbname=$target_db", $user, $password);
+            $this->_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
         }
-        echo "Connected successfully.";
+        catch (PDOException $exception){
+            echo "Connection failed" . $exception->getMessage();
+            $this->_connection = null;
+        }
+
+        return $this;
     }
 
-    public static function chooseSecurity(){
+    /**
+     * @param $table
+     * @param string $columns
+     * @return $this
+     */
+    public function select_From($table, $columns="*"){
+        try {
+            $this->_query = $this->_connection->prepare("SELECT $columns FROM $table");
+            $this->_query->execute();
 
+            $this->data = $this->_query->setFetchMode(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $exception){
+            echo "Error: " . $exception->getMessage();
+        }
+        return $this;
     }
 
-    public static function setStatements(){
-
+    /**
+     *
+     * @return this;
+     */
+    public function close_Connection(){
+        if ($this->_connection != null){
+            $this->_connection = null;
+        }
+        return this;
     }
 
 }
-
-
-
 ?>
